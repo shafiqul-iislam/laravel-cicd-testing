bda6876881d4a22a1ea3421494ac72bbb75afecc
Shafiqul Islam <shafiqulislam78652@gmail.com>
2025-05-29 09:22:49 +0600

    updating app

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ Route::get('/', function () {
 Route::get('/home', function () {
    $hello = '<h1>I am testing CI/CD with Laravel. I am so excited. I am so happy with this. love this. do you love this?</h1>';
 
-   $content = '<h1>Hello, This is Shafiq.</h1>';
+   $content = '<h1>Hello, This is Shafiq. I am a web developer.</h1>';
 
    return $hello . $content;
 });
